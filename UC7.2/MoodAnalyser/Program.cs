f879d251071b8f614e38b1982cab4a3554f14bb9
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-15 21:05:13 -0700

    7.2 Given_ImproperFieldName_Should_Throw_Exception

---
+++ UC7.2/MoodAnalyser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MoodAnalyser    // 7.2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); 
+        }
+
+        
+        }
+}
