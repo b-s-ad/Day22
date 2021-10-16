1b5530804f26ff34528520d2f38e98641cdbb978
b-s-ad <89061373+b-s-ad@users.noreply.github.com>
2021-10-15 20:38:11 -0700

    7.1 Given Happy message with reflector should return HAPPY

---
+++ UC7.1/MoodAnalyser/MoodAnalyserReflector.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyser        
+{
+    public class MoodAnalyserReflector  // 7.1
+    {
+        private string message;
+        public MoodAnalyserReflector(string message)
+        {
+            this.message = message;
+        }
+
+        public static string SetField(string message, string fieldName) //7.1
+        {
+            try
+            {
+                MoodAnalyser moodAnalyser = new MoodAnalyser();
+                Type type = typeof(MoodAnalyser);
+                FieldInfo field = type.GetField(fieldName, BindingFlags.Public | BindingFlags.Instance);
+                if (message == null)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_FIELD, "Message should not be null");
+                }
+                field.SetValue(moodAnalyser, message);
+                return moodAnalyser.message;
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_FIELD, "Field is not Found");
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
+
---
+++ UC7.1/MoodAnalyser/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace MoodAnalyser    // 7.1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine(); 
+        }
+
+        
+        }
+}
